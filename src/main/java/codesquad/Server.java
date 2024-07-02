d21c4e6b87a17c22085083c99318bfffe8345a15
hek-git <ghddmsrl1@naver.com>
2024-07-02 23:39:29 +0900

    feat: Server 클래스 구현 - 포트 개방 및 지정된 개수의 thread pool을 만들어 클라이언트 연결을 기다리는 Server 클래스 구현

---
+++ src/main/java/codesquad/Server.java
@@ -0,0 +1,42 @@
+package codesquad;
+
+import codesquad.handler.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final Logger log = LoggerFactory.getLogger(Server.class);
+    private final ServerSocket serverSocket;
+    private final ExecutorService executorService;
+
+    public Server(int port, int poolSize){
+        try {
+            serverSocket = new ServerSocket(port);
+            executorService = Executors.newFixedThreadPool(poolSize);
+            log.info("Listening for connection on port 8080 ....");
+        } catch (IOException e) {
+            log.info("could not create server socket");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void run() {
+
+        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
+            try {
+                Socket clientSocket = serverSocket.accept();  // 클라이언트 연결을 수락합니다.
+                log.info("Client connected");
+                executorService.submit(new RequestHandler(clientSocket));
+            } catch (IOException e) {
+                log.info("Cannot accept client connection");
+            }
+        }
+    }
+}
