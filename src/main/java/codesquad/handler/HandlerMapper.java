85d3a8712513e429f4125664e78b3352e4fde0d7
hek-git <ghddmsrl1@naver.com>
2024-07-05 05:08:02 +0900

    feat: 요청 정보에 따른 처리를 위해 적절한 Handler 반환하는 HandlerMapper 클래스 구현 - 정적 리소스 경로거나 디렉토리 경로인 경우 StaticResourceHandler 반환 - 경로에 /create 포함시 회원가입 기능이 있는 CreateHandler 반환 - 추가 Handler 구현하여 확장 가능

---
+++ src/main/java/codesquad/handler/HandlerMapper.java
@@ -0,0 +1,32 @@
+package codesquad.handler;
+
+import codesquad.util.ContentTypeMapper;
+import codesquad.util.DirectoryMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerMapper {
+
+    private static final Map<String, Handler> handlerMapper = new HashMap<>();
+
+    static {
+        handlerMapper.put("/create", new CreateHandler());
+    }
+
+    private HandlerMapper() {
+    }
+
+    public static Handler findHandler(String uri) {
+        if (ContentTypeMapper.supports(uri) || DirectoryMapper.isDirectory(uri)) {
+            return new StaticResourceHandler();
+        }
+
+        return handlerMapper.entrySet().stream()
+                .filter(entry -> uri.contains(entry.getKey()))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(null);
+    }
+
+}
