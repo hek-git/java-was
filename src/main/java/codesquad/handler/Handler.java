c30c4ef12e1078c2531fbc1416cfd30bde7b536a
hek-git <ghddmsrl1@naver.com>
2024-07-05 02:32:01 +0900

    feature : 요청을 실제로 처리해줄 handler interface 선언 - request 객체를 이용하여 response 반환하는 handle 추상 메서드 선언

---
+++ src/main/java/codesquad/handler/Handler.java
@@ -0,0 +1,9 @@
+package codesquad.handler;
+
+import codesquad.http.request.HttpRequest;
+import codesquad.http.response.HttpResponse;
+
+public interface Handler {
+
+    HttpResponse handle(HttpRequest request);
+}
