01f87530aa9a4256e1656c608d17df6f11211e81
hek-git <ghddmsrl1@naver.com>
2024-07-04 23:54:20 +0900

    feat: FileReader 클래스 변경 - FileReader 클래스에서 resourceName을 이용하여 File 클래스가 아닌 file 내용을 반환하도록 변경

--- src/main/java/codesquad/file/FileReader.java
+++ src/main/java/codesquad/file/FileReader.java
@@ -1,18 +1,44 @@
 package codesquad.file;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 
 public class FileReader {
 
-    private final String STATIC_PATH = "static";
+    private final static String STATIC_PATH = "static";
+    private final static Logger log = LoggerFactory.getLogger(FileReader.class);
 
-    public File getFile(String resourceName) {
-        URL resource = getClass().getClassLoader().getResource(STATIC_PATH + "/" + resourceName);
+    public static byte[] getContent(String resourceName) {
+
+        URL resource = FileReader.class.getClassLoader().getResource(STATIC_PATH + resourceName);
         if (resource == null) {
             throw new RuntimeException("Resource not found: " + resourceName);
         }
-        return new File(resource.getFile());
-    }
+        File file = new File(resource.getPath());
 
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            if (file.isFile() && file.exists()) {
+                byte[] content = new byte[(int) file.length()];
+                int read = fileInputStream.read(content);
+                if (read != file.length()) {
+                    throw new RuntimeException("could not read file");
+                }
+                fileInputStream.close();
+//                log.info("{}", new String(content));
+                return content;
+
+            } else {
+                throw new FileNotFoundException(file.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
