44de1af1c921232a3345e1771638d7492085170e
hek-git <ghddmsrl1@naver.com>
2024-07-11 13:32:09 +0900

    feat: user 클래스에 로그인 인증을 위한 getter 추가 - getPassword 메서드 추가 - getName 메서드 추가 - isNull 메서드 추가

--- src/main/java/codesquad/model/User.java
+++ src/main/java/codesquad/model/User.java
@@ -16,6 +16,18 @@ public class User {
         return userId;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isNull(){
+        return userId == null || password == null || name == null;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + userId + ", password=" + password + ", name=" + name + "]";
