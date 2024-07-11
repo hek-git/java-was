abcfdcfa8829e0ab0e988f3fe7307dc49cc82254
hek-git <ghddmsrl1@naver.com>
2024-07-11 09:44:55 +0900

    feat: RequestHandler 클래스 변경 - try-with-resource Statement로 변경

--- src/main/java/codesquad/server/RequestHandler.java
+++ src/main/java/codesquad/server/RequestHandler.java
@@ -15,23 +15,24 @@ public class RequestHandler implements Runnable {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private final InputStream inputStream;
-    private final OutputStream outputStream;
+    private final Socket clientSocket;
     private final HttpRequestParser httpRequestParser;
 
     public RequestHandler(Socket clientSocket) throws IOException {
-        this.inputStream = clientSocket.getInputStream();
-        this.outputStream = clientSocket.getOutputStream();
+        this.clientSocket = clientSocket;
         this.httpRequestParser = new HttpRequestParser();
     }
 
     @Override
     public void run() {
 
-        try {
-//             HttpRequest 클래스로 파싱하는 부분
-            HttpRequest request = httpRequestParser.parse(new BufferedReader(new InputStreamReader(inputStream)));
-//            log.info("{}\n", request.toString());
+        try (InputStream inputStream = clientSocket.getInputStream();
+             OutputStream outputStream = clientSocket.getOutputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))){
+
+            // HttpRequest 클래스로 파싱하는 부분
+            HttpRequest request = httpRequestParser.parse(reader);
+//            log.info("{}", request.toString());
             log.info("Request Method : {}", request.method());
             log.info("Request URI : {}", request.path());
             log.info("Request Body: {}\n", new String(request.body()));
@@ -39,9 +40,6 @@ public class RequestHandler implements Runnable {
             // HandlerMapper 이용하여 요청을 처리할 수 있는 Handler 찾고 요청 handling
             Handler handler = HandlerMapper.findHandler(request.path());
             if (handler == null) {
-                outputStream.flush();
-                outputStream.close();
-                inputStream.close();
                 throw new RuntimeException("Cannot handle request");
             }
             HttpResponse response = handler.handle(request);
@@ -49,15 +47,9 @@ public class RequestHandler implements Runnable {
 //             HttpResponse 인스턴스를 반환하는 부분
             outputStream.write(response.toByteArray());
             outputStream.write(response.getBody());
-
             outputStream.flush();
-            outputStream.close();
-            inputStream.close();
-
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-
     }
-
 }
